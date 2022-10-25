c2fd40cf8fda8c4c612a54409b3b5fe46be4b097
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-10-25 18:55:18 +0530

    Question 4

--- 1014_abhinandan_Q4.c
+++ 1014_abhinandan_Q4.c
@@ -3,6 +3,22 @@
 int main() {
     printf("\nAbhinandan Wadhwa - 1014 \n\n\n");
 
+    float oldPrice, repairAmt, sellingPrice;
+    printf("Price of old scooter:\n");
+    scanf("%f", &oldPrice);
+    
+    printf("Repair Amount:\n");
+    scanf("%f", &repairAmt);
+
+    float costPrice = oldPrice + repairAmt;
+    
+    printf("Selling Price:\n");
+    scanf("%f", &sellingPrice);
+
+    float gainPercentage = ((sellingPrice-costPrice)/costPrice) * (100.0);
+
+    printf("Gain percentage is %f\n", gainPercentage);
+
     
     return 0;
 }
